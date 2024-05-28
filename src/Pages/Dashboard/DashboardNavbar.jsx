9ed7810b11cba311e0ee092b78cd96d43278ad05
Avinash Shiyani <avi_24@aptrix.in>
2024-05-28 19:42:36 +0530

    new : new navbar added

--- src/Pages/Dashboard/DashboardNavbar.jsx
+++ src/Pages/Dashboard/DashboardNavbar.jsx
@@ -1,84 +1,50 @@
-import React from 'react';
-import DashboardNavigation from '../../Components/DashboardNavigation'
-import Button from '../../Components/Button';
-
+import React from "react";
+import { Link } from "react-router-dom";
+// import DashboardNavigation from "../../Components/DashboardNavigation";
+// import Button from "../../Components/Button";
+import ApplicationLogo from "../../Components/ApplicationLogo";
 
 const DashboardNavbar = () => {
+  // sending data in array
+  const navigationItems = [
+    { path: "dashboard", label: "Dashboard" },
+    { path: "suppeliers", label: "Suppliers" },
+    { path: "product", label: "Product" },
+    { path: "invoices", label: "Invoices" },
+    // { path: "stocks", label: "Stocks" },
+    // { path: "transition", label: "Transition" },
+    // { path: "slips", label: "Slips" },
+  ];
 
-    // sending data in array
-    const navigationItems = [
-        { path: 'dashboard', label: 'Dashboard' },
-        { path: 'suppeliers', label: 'Suppliers' },
-        { path: 'department', label: 'Department' },
-        { path: 'stocks', label: 'Stocks' },
-        { path: 'transition', label: 'Transition' },
-        { path: 'slips', label: 'Slips' },
-        { path: 'invoices', label: 'Invoices' }
-    ];
-
-    return (
-        <div className="px-4 mx-auto max-w-7xl sm:px-6">
-            <div className="relative pt-6 pb-16 sm:pb-24">
-                <nav
-                    className="relative flex items-center justify-between sm:h-10 md:justify-center"
-                    aria-label="Global"
+  return (
+    <div className="grid">
+      <div className="flex ">
+        {/* navbar */}
+        <nav className="flex  items-center h-[74px]  w-full">
+          {/* application logo */}
+          <div className="flex justify-center items-center">
+            <ApplicationLogo className="text-[#1F2937] h-[36px] w-[36px] ml-[48px]" />
+          </div>
+          {/* navbar links */}
+          <div className="flex h-full pl-[62px] space-x-[62px]">
+            {navigationItems.map((data, index) => (
+              <div
+                key={index}
+                className="h-full flex justify-center items-center "
+              >
+                <Link
+                  className="flex items-center border-b-2 border-[#5E68C9] h-full "
+                  to={`/${data.path}`}
                 >
-                    <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
-                        <div className="flex items-center justify-between w-full md:w-auto">
-                            <a href="#">
-                                <span className="sr-only">Comspany Name</span>
-                                <img
-                                    className="w-auto h-8 sm:h-10"
-                                    src="https://www.svgrepo.com/show/448244/pack.svg"
-                                    loading="lazy"
-                                    width={202}
-                                    height={40}
-                                />
-                            </a>
-                            <div className="flex items-center -mr-2 md:hidden">
-                                <button
-                                    className="inline-flex items-center justify-center p-2 text-gray-400 bg-gray-50 rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-50"
-                                    type="button"
-                                    aria-expanded="false"
-                                >
-                                    <span className="sr-only">Open main menu</span>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={2}
-                                        stroke="currentColor"
-                                        aria-hidden="true"
-                                        className="w-6 h-6"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M4 6h16M4 12h16M4 18h16"
-                                        />
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="hidden md:flex md:space-x-10 list-none">
-                        {/* rendering the data and navigation both */}
-                        {navigationItems.map((item) => (
-                            <DashboardNavigation key={item.path} path={item.path}>
-                                {item.label}
-                            </DashboardNavigation>
-                        ))}
-                    </div>
-                    <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
-                        <div className="">
-                            <Button >Sign Out</Button>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+                  {data.label}
+                </Link>
+              </div>
+            ))}
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default DashboardNavbar
+export default DashboardNavbar;
