1eb2911ea9b0a6f3c766bee6020a82948dbffdcf
Avinash Shiyani <avi_24@aptrix.in>
2024-05-28 17:13:22 +0530

    formatted

--- src/Pages/Dashboard/DashboardInner.jsx
+++ src/Pages/Dashboard/DashboardInner.jsx
@@ -1,34 +1,30 @@
-import React from 'react';
-import DashboardBox from './DashboardBox';
-import DashboardTable from './DashboardTable';
+import React from "react";
+import DashboardBox from "./DashboardBox";
+import DashboardTable from "./DashboardTable";
 
 const DashboardInner = () => {
-
-    // represeting total number of dashboard boxes and their name and data
-    const totalDashboardBox = [
-        { name: 'Products', innerData: 1099 },
-        { name: 'Department', innerData: 24 },
-        { name: 'Suppeliers', innerData: 23 },
-        { name: 'Invoices', innerData: 3447 },
-    ]
-    return (
-        <>
-            <div className="p-4 -mt-16  bg-gray-200">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-
-                    {/*  rendering the component with name and data */}
-                    {totalDashboardBox.map((e, index) => (
-                        <div key={index}>
-                            <DashboardBox name={e.name} innerData={e.innerData} />
-                        </div>
-                    ))}
-
-                </div>
+  // represeting total number of dashboard boxes and their name and data
+  const totalDashboardBox = [
+    { name: "Products", innerData: 1099 },
+    { name: "Department", innerData: 24 },
+    { name: "Suppeliers", innerData: 23 },
+    { name: "Invoices", innerData: 3447 },
+  ];
+  return (
+    <>
+      <div className="p-4 -mt-16  bg-gray-200">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          {/*  rendering the component with name and data */}
+          {totalDashboardBox.map((e, index) => (
+            <div key={index}>
+              <DashboardBox name={e.name} innerData={e.innerData} />
             </div>
-            <DashboardTable />
-        </>
+          ))}
+        </div>
+      </div>
+      <DashboardTable />
+    </>
+  );
+};
 
-    )
-}
-
-export default DashboardInner
+export default DashboardInner;
