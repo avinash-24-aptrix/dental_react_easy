6c4589770335994fcaa3c09fab3b15fd28da6800
Avinash Shiyani <avi_24@aptrix.in>
2024-05-28 19:42:19 +0530

    Update : new navbar in innerDashboard

--- src/Pages/Dashboard/DashboardInner.jsx
+++ src/Pages/Dashboard/DashboardInner.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DashboardBox from "./DashboardBox";
+import DashboardBox from "../../Components/DashboardBox";
 import DashboardTable from "./DashboardTable";
 
 const DashboardInner = () => {
@@ -8,12 +8,12 @@ const DashboardInner = () => {
     { name: "Products", innerData: 1099 },
     { name: "Department", innerData: 24 },
     { name: "Suppeliers", innerData: 23 },
-    { name: "Invoices", innerData: 3447 },
+    { name: "Invoices", innerData: 347 },
   ];
   return (
     <>
-      <div className="p-4 -mt-16  bg-gray-200">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="py-4 bg-[#F3F4F6]">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[20px] px-[48px]">
           {/*  rendering the component with name and data */}
           {totalDashboardBox.map((e, index) => (
             <div key={index}>
@@ -22,7 +22,7 @@ const DashboardInner = () => {
           ))}
         </div>
       </div>
-      <DashboardTable />
+      {/* <DashboardTable /> */}
     </>
   );
 };
