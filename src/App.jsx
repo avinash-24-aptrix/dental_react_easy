fa370f80dee47f42d3e0145b005ec8d5e4479685
Avinash Shiyani <avi_24@aptrix.in>
2024-05-28 17:12:12 +0530

    unlocking login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Dashboard from './Pages/Dashboard/Dashboard'
-// import LoginPage from './Pages/LoginPage'
-import RegistrationPage from './Pages/RegistrationPage'
-import DashboardNavbar from './Pages/Dashboard/DashboardNavbar'
-import PageNotFound from './Pages/PageNotFound';
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import LoginPage from "./Pages/LoginPage";
+// import RegistrationPage from './Pages/RegistrationPage'
+
+import PageNotFound from "./Pages/PageNotFound";
+
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <DashboardNavbar />
+       
         <Routes>
-          {/* <Route exact path='/' element={<LoginPage />}></Route> */}
+          <Route exact path="/" element={<LoginPage />}></Route>
           {/* <Route exact path='/register' element={<RegistrationPage />}></Route> */}
-          <Route  path='/:params' element={<Dashboard />}></Route>
-          <Route  path='*' element={<PageNotFound />}></Route>
+          <Route path="/:params" element={<Dashboard />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
