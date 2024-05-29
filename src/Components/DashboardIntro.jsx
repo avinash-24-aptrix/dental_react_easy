2ca5caa182e6dd8c17aac89da142d526715908e9
Avinash Shiyani <avi_24@aptrix.in>
2024-05-29 11:17:28 +0530

    remove : background color

--- src/Components/DashboardIntro.jsx
+++ src/Components/DashboardIntro.jsx
@@ -5,7 +5,7 @@ const DashboardIntro = ({ className = "bg-white", pageName = "Dashboard" }) => {
   return (
     <div
       className={twMerge(
-        "flex items-center h-[74px] text-[20px] font-medium bg-gray-300 pl-[48px]",
+        "flex items-center h-[74px] text-[20px]  font-medium  pl-[48px]",
         className
       )}
     >
