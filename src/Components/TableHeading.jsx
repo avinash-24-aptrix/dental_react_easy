367c6e9ad064036b30accd3c414ad64d2413a84c
Avinash Shiyani <avi_24@aptrix.in>
2024-05-30 10:52:18 +0530

    table heading icon addedd

--- src/Components/TableHeading.jsx
+++ src/Components/TableHeading.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { twMerge } from 'tailwind-merge'
+import React from "react";
+import { twMerge } from "tailwind-merge";
+import { RiExpandUpDownFill } from "react-icons/ri";
 
-const TableHeading = ({ scope, className , children }) => {
-    return (
-        <>
-            <th scope={scope} className={twMerge("px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider", className)}>
-                {children}
-            </th>
-        </>
-    )
-}
+const TableHeading = ({ scope, className, children }) => {
+  return (
+    <>
+      <th
+        scope={scope}
+        className={twMerge(
+          "px-6  text-left text-xs font-medium  text-gray-800 bg-[#D1D5DB] h-[40px] uppercase tracking-wider font-bold",
+          className
+        )}
+      >
+        <span className="flex items-center cursor-pointer">
+          {children}
+          <RiExpandUpDownFill className="" />
+        </span>
+      </th>
+    </>
+  );
+};
 
-export default TableHeading
+export default TableHeading;
