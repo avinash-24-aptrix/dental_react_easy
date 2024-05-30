a5f2d2ece8672a65dab87ac2a1309de849240d28
Avinash Shiyani <avi_24@aptrix.in>
2024-05-30 13:02:39 +0530

    update : dynamic routing

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Suppelier from "../Suppelier/Suppelier";
-import DashboardInner from "./DashboardInner";
 import DashboardNavbar from "./DashboardNavbar";
 import DashboardIntro from "../../Components/DashboardIntro";
-import DashboardTable from '../Dashboard/DashboardTable';
+import OpenDashboard from "./OpenDashboard";
 
 const Dashboard = () => {
   //using useparams hook to get data from URL
   const params = useParams();
   const openParameter = params.params;
+
+  const viewPagefromUrl = {
+    dashboard: <OpenDashboard />,
+    suppeliers: <Suppelier />,
+    default: "no bro no",
+  };
+
+  const openPage = viewPagefromUrl
+    ? viewPagefromUrl[openParameter]
+    : viewPagefromUrl.default;
+  console.log(openPage);
   return (
     <div>
       <DashboardNavbar />
       <DashboardIntro pageName={openParameter} />
-      <DashboardInner />
-      <DashboardTable />
+      {openPage}
     </div>
   );
 };
