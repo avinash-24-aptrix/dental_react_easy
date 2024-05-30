14b2f298385f74e9a35915c303b19a7c8237f3b1
Avinash Shiyani <avi_24@aptrix.in>
2024-05-30 13:04:22 +0530

    update : added styles

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <div className='fixed h-screen w-screen flex justify-center items-center text-[30px]'>
+      Page Not Found
+    </div>
   )
 }
 
