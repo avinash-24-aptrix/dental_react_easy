d2830195eaf7a4ebec2e98e3af58507ca6253afd
Avinash Shiyani <avi_24@aptrix.in>
2024-05-30 13:02:22 +0530

    new : more dynmaic components

--- src/Components/DashboardIntro.jsx
+++ src/Components/DashboardIntro.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
+import Button from "../Components/Button";
 
 const DashboardIntro = ({ className = "bg-white", pageName = "Dashboard" }) => {
+  const displayRenderedComponent = () => {
+    switch (pageName) {
+      case "suppeliers":
+        return (
+          <div>
+            {pageName} <Button className="bg-[#1956DA]">Add Supplier + </Button>
+          </div>
+        );
+      // Add more cases as needed
+      default:
+        return <div className="capitalize">{pageName}</div>;
+    }
+  };
   return (
-    <div
-      className={twMerge(
-        "flex items-center h-[74px] text-[20px]  font-medium  pl-[48px]",
-        className
-      )}
-    >
-      <span className="capitalize">{pageName}</span>
-    </div>
+    <>
+      <div
+        className={twMerge(
+          "flex items-center h-[74px] text-[20px] font-medium pl-[48px] capitalize",
+          className
+        )}
+      >
+        {displayRenderedComponent()}
+      </div>
+    </>
   );
 };
 
